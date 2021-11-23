6408a7791a8b4312707cfc1f56b358421f3706e1
Umer Arshad <umerm64@gmail.com>
2021-11-23 21:05:18 +0500

    JWT authentication

--- advanced_course/flask_restful_example.py
+++ advanced_course/flask_restful_example.py
@@ -1,18 +1,24 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'jose'
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity) # /auth
 
 items = []
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name']==name, items), None)
         return {'item': item}, 200 if item else 404
 
     def post(self, name):
-        if next(filter(lambda x: x['name']==name, items), None) is not None:
+        if next(filter(lambda x: x['name']==name, items), None):
             return {'message': "An item with name '{}' already exists".format(name)}, 400
 
         data = request.get_json()
