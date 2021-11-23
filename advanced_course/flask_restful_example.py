d2d1e54fc1e2d5141ccef8db476d979bbbf26870
Umer Arshad <umerm64@gmail.com>
2021-11-23 21:23:01 +0500

    put request

--- advanced_course/flask_restful_example.py
+++ advanced_course/flask_restful_example.py
@@ -31,6 +31,18 @@ class Item(Resource):
         items = list(filter(lambda x: x['name']!=name, items))
         return {'message': 'Item deleted'}
 
+    def put(self, name):
+        data = request.get_json()
+        item = next(iter(filter(lambda x: x['name']==name, items)), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+
+        return item
+
+
 class ItemList(Resource):
     def get(self):
         return {'items': items}
